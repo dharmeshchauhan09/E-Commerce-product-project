383e2d0f8ed691726de8fe897e2d6184a119f689
Dharmesh Chauhan <dharmeshchauhand44@gmail.com>
2025-05-20 13:26:52 +0530

    Add files via upload

---
+++ models/index.js
@@ -0,0 +1,11 @@
+const User = require('./userModel');
+const Product = require('./productModel');
+const Order = require('./orderModel');
+const Cart = require('./cartModel');
+
+module.exports = {
+  User,
+  Product,
+  Order,
+  Cart,
+};
